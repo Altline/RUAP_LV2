cf8579327cad93886f86579f04d3cab71bacc57e
Dominik Živko <domy.zivko@gmail.com>
2021-11-19 23:07:57 +0100

    Adding Data-Persistence Features to the Contact Repository Implementation

--- Begin/ContactManager/Services/ContactRepository.cs
+++ Begin/ContactManager/Services/ContactRepository.cs
@@ -4,21 +4,40 @@ namespace ContactManager.Services
 {
     public class ContactRepository
     {
+        // HttpContext does not have Cache in this version. This is a quick replacement
+        private Contact[] contacts;
+
+        public ContactRepository()
+        {
+            contacts = new Contact[]
+            {
+                new Contact
+                {
+                    Id = 1, Name = "Glenn Block"
+                },
+                new Contact
+                {
+                    Id = 2, Name = "Dan Roth"
+                }
+            };
+        }
+
         public Contact[] GetAllContacts()
         {
-            return new Contact[]
+            return contacts;
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            try
             {
-            new Contact
-            {
-                Id = 1,
-                Name = "Glenn Block"
-            },
-            new Contact
-            {
-                Id = 2,
-                Name = "Dan Roth"
+                contacts.Append(contact);
+                return true;
             }
-            };
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
